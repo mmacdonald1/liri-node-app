183f728d70f5a87dff159144c40e85a78e20722f
Melanie MacDonald <melmacd94@gmail.com>
2017-11-15 18:54:25 -0500

    files ignore

--- keys.js
+++
@@ -1,7 +0,0 @@
-console.log('this is loaded');
-exports.twitterKeys = {
-  consumer_key: 'SSH67JkiZBPoHfjCmjNObNMWD',
-  consumer_secret: '3pB1tX92zMOFWHqRNsO3rFnV4RmD2EzwpfWb0Hv4dXUt66KUOl',
-  access_token_key: '930607230059245568-CsSL3xCQ7oII8wnUUaMm5KfQVWF5OZb',
-  access_token_secret: 'zKxqRQYKhpeGFGZf9g0wq0UOKYmtBpnQSf3l9yFfP8HDL',
-};
